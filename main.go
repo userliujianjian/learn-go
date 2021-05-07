632971a23fdc318cc5e54e67750223b4f6b4f7cf
edz <sxliujianjian@163.com>
2021-05-07 10:45:30 +0800

    基于 errgroup 实现一个 http server 的启动和关闭 ，以及 linux signal 信号的注册和处理，要保证能够一个退出，全部注销退出

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"golang.org/x/sync/errgroup"
 	"log"
@@ -19,11 +18,7 @@ func main() {
 	// use errGroup 创建一个启动任务
 	g, _ := errgroup.WithContext(context.Background())
 	g.Go(ServerDebug)
-	fmt.Println("111111")
 	g.Go(ServerApp)
-	fmt.Println("22222")
-	g.Go(Signal)
-	fmt.Println("333333")
 
 	// 保持接收
 	if err := g.Wait(); err != nil {
@@ -33,42 +28,45 @@ func main() {
 
 }
 
-func Signal() error {
-	// 接收 linux signal 信号
-	signalChan := make(chan os.Signal)
-	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
-	sig := <-signalChan
-	log.Printf("Get Signal: %v\n", sig)
-	//ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-	//ctx.Done()
-	//defer cancel()
-	//log.Printf("ctx : %v\n", ctx)
-	// TODO shutdown server 结束server
-	return errors.New("asdf")
+func Server(addr string, handler http.Handler) error {
+	s := http.Server{
+		Addr:    addr,
+		Handler: handler,
+	}
+
+	go func() {
+		// 接收 linux signal 信号
+		signalChan := make(chan os.Signal)
+		signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+		sig := <-signalChan
+		log.Printf("Get Signal: %v\n", sig)
+		s.Shutdown(context.Background())
+	}()
+	fmt.Printf("[server] address %v\n", addr)
+
+	return s.ListenAndServe()
+
 }
 
 func ServerApp() error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "hello GopherCon SG")
-		// 四舍五入 保留两位小数
-		var (
-			a = 100
-			b = 100
-		)
-
-		fmt.Printf("res: %v\n", 1/a-b)
+	})
+	mux.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		dayStr := ""
 		_, err := time.ParseInLocation("20060102150405", dayStr, time.Local)
 		if err != nil {
 			fmt.Printf("format err : %v\n", err)
 			log.Fatal(err)
 		}
-
 	})
-	return http.ListenAndServe("0.0.0.0:8080", mux)
+	addr := "0.0.0.0:8080"
+	return Server(addr, mux)
 }
 
 func ServerDebug() error {
-	return http.ListenAndServe("127.0.0.1:8000", http.DefaultServeMux)
+	mux := http.DefaultServeMux
+	addr := "127.0.0.1:8009"
+	return Server(addr, mux)
 }
